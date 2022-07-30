7d997fa4c107fe049784d5390818da226508f350
tikmart <t.martirosyan012@gmail.com>
2022-07-30 16:49:53 +0400

    initial(incomplete task)

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -1,7 +1,29 @@
 package school.mjc.stage0.loops.task5;
 
 public class Cross {
-    public void printCross(int sideLength) {
+    public static void printCross(int sideLength) {
 
+        int i,j;
+        if (sideLength % 2 != 0) {
+            for (i = 0; i < sideLength; i++) {
+
+                for (j = 0; j < sideLength; j++) {
+                    if ((i ==(int) sideLength / 2) || (j == (int) sideLength / 2)) {
+                        System.out.print(8);
+                    } else {
+                        System.out.print(" ");
+                    }
+
+                }
+
+                System.out.println();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        printCross(5);
+        printCross(3);
+        printCross(0);
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Square.java
+++ src/main/java/school/mjc/stage0/loops/task5/Square.java
@@ -1,7 +1,36 @@
 package school.mjc.stage0.loops.task5;
 
 public class Square {
-    public void printSquareFrom8s(int sideLength){
+    public static void printSquareFrom8s(int sideLength){
+        int i,j;
+        for (i = 1; i <= sideLength; i++) {
 
+
+
+            for (j = 1; j <= sideLength; j++) {
+                  if (i == 1 || i == sideLength ||
+                  j == 1 || j == sideLength) {
+                      System.out.print(8);
+
+                } else {
+                      System.out.print(" ");
+                  }
+
+
+            }
+
+            System.out.println();
+
+
+            }
+
+        }
+
+
+
+    public static void main(String[] args) {
+        printSquareFrom8s(5);
+        printSquareFrom8s(2);
+        printSquareFrom8s(0);
     }
 }
