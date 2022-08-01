a841d0034d801bd306aff08d3c90a1a11d868112
tikmart <t.martirosyan012@gmail.com>
2022-08-01 23:38:25 +0400

    initial complete

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -1,7 +1,17 @@
 package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
-    public void printHourglassOfGivenSize(int height) {
+    public static void printHourglassOfGivenSize(int height) {
+
+        for (int i = 1; i <= height ; i++) {
+
+
+        }
+        }
+
+    public static void main(String[] args) {
+        printHourglassOfGivenSize(5);
+    }
 
     }
-}
+
--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -1,7 +1,28 @@
 package school.mjc.stage0.loops.task5;
 
 public class Triangle {
-    public void printTriangle(int cathetusLength) {
+    public static void printTriangle(int cathetusLength) {
 
+
+        for (int i = 1; i <=cathetusLength ; i++) {
+
+
+            for (int j = 1; j <= i ; j++) {
+                System.out.print(8);
+            }
+
+            for (int s = 1; s <= cathetusLength - i ; s++) {
+                System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        printTriangle(5);
+        printTriangle(3);
     }
 }
